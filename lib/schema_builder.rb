486c60ebc7872d112501d05452dbf42f8f728e5a
arikarim <arikarim146@gmail.com>
2022-12-10 04:51:10 +0300

    first commit

---
+++ lib/schema_builder.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "schema_builder/version"
+
+module SchemaBuilder
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/schema_builder_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SchemaBuilder do
+  it "has a version number" do
+    expect(SchemaBuilder::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
